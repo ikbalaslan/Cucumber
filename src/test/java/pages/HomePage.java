556193c12a106c44ae9532e40e6cb351e545162b
Ikbal Aslan <ikball.aslann@gmail.com>
2023-01-31 19:50:48 +0300

    Upd-4

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HomePage {
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "dropdown-basic-button")
+    public WebElement userID;
+
+    @FindBy(xpath = "//*[@class='btn btn-primary btn-sm']")
+    public WebElement loginButton;
+
+    @FindBy(partialLinkText = "Login")
+    public WebElement homePageLoginLink;
+
+    @FindBy(linkText = "Logout")
+    public WebElement logoutLink;
+
+    @FindBy(xpath = "//button[text()='OK']")
+    public WebElement OK;
+
+
+
+}
